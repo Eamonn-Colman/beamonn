31540e2fb6caf963457b2a014b06057b034af715
unknown <eamonn@computenext.com>
2015-08-27 14:14:08 -0700

    Fixed wp-config

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'eamonncoDBpe2nm');
+define('DB_NAME', 'eamonncoDBb28s');
 
 /** MySQL database username */
-define('DB_USER', 'eamonncoDBpe2nm');
+define('DB_USER', 'eamonncoDBb28s');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Kt31QcCNnk');
+define('DB_PASSWORD', 'yit1kit1L3');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
